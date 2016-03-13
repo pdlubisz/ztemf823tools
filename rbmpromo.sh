a2d07daadb7370f82158110b0aeb128f529db337
Piotr Dłubisz <pdlubisz@gmail.com>
2016-03-13 21:54:43 +0100

    added promo service

--- rbmpromo.sh
+++ rbmpromo.sh
@@ -19,13 +19,9 @@ fi
 
 sleep 1
 
-#pdlubisz@malina:~/git/ztemf823tools $ ./rbmpromo.sh
 #{"result":"success"}{"result":"success"}{"result":"success"}{"result":"success"}{"ussd_write_flag":"15"}{"ussd_write_flag":"15"}{"ussd_write_flag":"16"}Dziekujemy za zgloszenie! Twoja usluga niedlugo zostanie wlaczona, moze nam to zajac maksymalnie do 24 godzin. Wiecej na naszej stronie WWW.
 #0 Wyjscie{"result":"success"}{"result":"success"}{"result":"success"}pdlubisz@malina:~/git/ztemf823tools $
 
-
-
-#pdlubisz@malina:~/git/ztemf823tools $ ./rbmpromo.sh
 #{"result":"success"}{"result":"success"}{"result":"success"}{"result":"success"}{"ussd_write_flag":"15"}{"ussd_write_flag":"16"}{"ussd_write_flag":"16"}Usluga w trakcie wlaczania
 #0 Wyjscie{"result":"success"}{"result":"success"}{"result":"success"}pdlubisz@malina:~/git/ztemf823tools $
 
@@ -53,6 +49,7 @@ elif ( echo $USSD_RESP | grep -q  "Usluga w trakcie wlaczania" )
 then
   RESULT=1
 else
+  SEND_STATUS=$(curl -s -0 "http://192.168.0.1/goform/goform_set_cmd_process" -H "Host: 192.168.0.1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0" -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Accept-Language: en-US,en;q=0.5" --compressed -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" -H "X-Requested-With: XMLHttpRequest" -H "Referer: http://192.168.0.1/index.html" -H "Connection: keep-alive" -H "Pragma: no-cache" -H "Cache-Control: no-cache" --data "isTest=false&goformId=USSD_PROCESS&USSD_operator=ussd_send&USSD_send_number=*111*480*1"%"23&notCallback=true")
   RESULT=1
 fi
 
@@ -65,4 +62,3 @@ sleep 2
 curl -s 'http://192.168.0.1/goform/goform_set_cmd_process' -H 'Host: 192.168.0.1' -H 'Referer: http://192.168.0.1/index.html' --compressed --data 'goformId=CONNECT_NETWORK'
 
 exit $RESULT
-
